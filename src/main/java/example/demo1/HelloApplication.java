d9d48614841ad084e9683476af8df468b57c2f7b
ProgrammingSis <saraleticia5170@gmail.com>
2024-11-24 02:01:14 -0300

    feat: Tela inicial do sistema foi criado

--- src/main/java/example/demo1/HelloApplication.java
+++ src/main/java/example/demo1/HelloApplication.java
@@ -1,23 +1,17 @@
 package example.demo1;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
-        stage.setTitle("VetCare");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        ScreenManager screenManager = new ScreenManager(stage);
+        screenManager.switchScreen("/example/demo1/telaInicio.fxml");
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/example/demo1/HelloController.java
+++ src/main/java/example/demo1/HelloController.java
@@ -1,14 +1,27 @@
 package example.demo1;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button iniciarSessaoButton;
 
+    public void initialize() {
+        iniciarSessaoButton.setOnAction(event -> switchToAnotherScene());
+    }
+
+    //Switches the scene as soon as the button "Iniciar Sessão" is clicked
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switchToAnotherScene(){
+        ScreenManager screenManager = new ScreenManager(new Stage());
+        screenManager.switchScreen("/example/demo1/TelaInicio.fxml");
     }
 }
